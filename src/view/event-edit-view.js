52b92e8f530674fc1cca225212b84c9d42c89fd3
Yuriy Marin <yuriy.marin@gmail.com>
2020-09-10 22:00:05 +0300

    refactior: Вывод шаблонов сводки и редактирования разбит на методы
    Чтобы код был более модульным и лучше читался, в отображениях
    EventEdit и EventSummary вывод шалона разбит на несколько разных
    методов.
    
    Из мок-данных убрано свойство isTransfer, т.к. это вычисляемое
    свойство.

--- src/view/event-edit-view.js
+++ src/view/event-edit-view.js
@@ -10,7 +10,6 @@ const DEFAULT_EVENT_VALUES = {
   endTime: new Date().setHours(new Date().getHours() + 2, 0, 0, 0),
   price: 0,
   offers: [],
-  isTransfer: true,
   isFavorite: false
 };
 
@@ -21,115 +20,38 @@ export class EventEditView extends AbstractView {
 
     this._closeHandler = this._closeHandler.bind(this);
     this._submitHandler = this._submitHandler.bind(this);
+    this._toggleFavoriteHandler = this._toggleFavoriteHandler.bind(this);
   }
 
   get template() {
-    const {id, type, isTransfer, isFavorite, city, beginTime, endTime, price, offers} = this._event;
-    const offersList = (isTransfer) ? TRANSPORT_OFFERS_MAP : STOP_OFFERS_MAP;
+    const eventTypeSelector = this._makeEventTypeSelector();
+    const citySelector = this._makeCitySelector();
+    const timeInput = this._makeTimeInput();
+    const priceInput = this._makePriceInput();
+    const favoriteButton = this._makeFavoriteButton();
+    const offersList = this._makeOffersList();
 
-    let template = `<form class="event event--edit" action="#" method="post">
-                    <header class="event__header">
-                      <div class="event__type-wrapper">
-                        <label class="event__type  event__type-btn" for="event-type-toggle-${id}">
-                          <span class="visually-hidden">Choose event type</span>
-                          <img class="event__type-icon" width="17" height="17" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
-                        </label>
-                        <input class="event__type-toggle  visually-hidden" id="event-type-toggle-${id}" type="checkbox">
+    return `<form class="event event--edit" action="#" method="post">
+        <header class="event__header">
+          ${eventTypeSelector}
 
-                        <div class="event__type-list">
-                          <fieldset class="event__type-group">
-                            <legend class="visually-hidden">Transfer</legend>`;
+          ${citySelector}
 
-    for (const transferName of TRANSPORTS) {
-      template += `<div class="event__type-item">
-                     <input id="event-type-${transferName.toLowerCase()}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${transferName.toLowerCase()}" ${(transferName === type) ? `checked` : ``}>
-                     <label class="event__type-label  event__type-label--${transferName.toLowerCase()}" for="event-type-${transferName.toLowerCase()}-1">${transferName}</label>
-                   </div>`;
-    }
+          ${timeInput}
 
-    template += `</fieldset>
-                   <fieldset class="event__type-group">
-                   <legend class="visually-hidden">Activity</legend>`;
+          ${priceInput}
 
-    for (const activityName of STOPS) {
-      template += `<div class="event__type-item">
-                     <input id="event-type-${activityName.toLowerCase()}-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="${activityName.toLowerCase()}" ${(activityName === type) ? `checked` : ``}>
-                     <label class="event__type-label  event__type-label--${activityName.toLowerCase()}" for="event-type-${activityName.toLowerCase()}-1">${activityName}</label>
-                   </div>`;
-    }
+          <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
+          <button class="event__reset-btn" type="reset">Delete</button>
 
-    template += `</fieldset>
-                          </div>
-                        </div>
+          ${favoriteButton}
 
-                        <div class="event__field-group  event__field-group--destination">
-                          <label class="event__label  event__type-output" for="event-destination-${id}">
-                            ${type} ${(isTransfer) ? `to` : `in`}
-                          </label>
-                          <input class="event__input  event__input--destination" id="event-destination-${id}" type="text" name="event-destination" value="${city}" list="destination-list-1">
-                          <datalist id="destination-list-${id}">`;
-
-    CITIES.forEach((cityName) => {
-      template += `<option value="${cityName}"></option>`;
-    });
-
-    template += `</datalist>
-                        </div>
-
-                        <div class="event__field-group  event__field-group--time">
-                          <label class="visually-hidden" for="event-start-time-${id}">
-                            From
-                          </label>
-                          <input class="event__input  event__input--time" id="event-start-time-${id}" type="text" name="event-start-time" value="${beginTime.toLocaleDateString(`en-GB`, {year: `2-digit`, month: `2-digit`, day: `2-digit`})} ${beginTime.toLocaleTimeString(`en-GB`, {timeStyle: `short`, hour12: false})}">
-                          &mdash;
-                          <label class="visually-hidden" for="event-end-time-${id}">
-                            To
-                          </label>
-                          <input class="event__input  event__input--time" id="event-end-time-${id}" type="text" name="event-end-time" value="${endTime.toLocaleDateString(`en-GB`, {year: `2-digit`, month: `2-digit`, day: `2-digit`})} ${endTime.toLocaleTimeString(`en-GB`, {timeStyle: `short`, hour12: false})}">
-                        </div>
-
-                        <div class="event__field-group  event__field-group--price">
-                          <label class="event__label" for="event-price-${id}">
-                            <span class="visually-hidden">Price</span>
-                            &euro;
-                          </label>
-                          <input class="event__input  event__input--price" id="event-price-${id}" type="text" name="event-price" value="${price}">
-                        </div>
-
-                        <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
-                        <button class="event__reset-btn" type="reset">Delete</button>
-
-                        <input id="event-favorite-${id}" class="event__favorite-checkbox  visually-hidden" type="checkbox" name="event-favorite" ${(isFavorite) ? `checked` : ``}>
-                        <label class="event__favorite-btn" for="event-favorite-${id}">
-                          <span class="visually-hidden">Add to favorite</span>
-                          <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
-                            <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
-                          </svg>
-                        </label>
-
-                        <button class="event__rollup-btn" type="button">
-                          <span class="visually-hidden">Open event</span>
-                        </button>
-                      </header>`;
-    if (city.length) {
-      template += `<section class="event__details">
-                        <section class="event__section  event__section--offers">
-                          <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-                          <div class="event__available-offers">`;
-      for (const [offerId, offerDetails] of offersList) {
-        template += `<div class="event__offer-selector">
-                      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offerId}-${id}" type="checkbox" name="event-offer-${offerId}" ${(offers[offerId]) ? `checked` : ``}>
-                      <label class="event__offer-label" for="event-offer-${offerId}-${id}">
-                        <span class="event__offer-title">${offerDetails.title}</span>
-                        &plus;
-                        &euro;&nbsp;<span class="event__offer-price">${offerDetails.price}</span>
-                      </label>
-                  </div>`;
-      }
-      template += `</section></section>`;
-    }
-    template += `</form>`;
-    return template;
+          <button class="event__rollup-btn" type="button">
+            <span class="visually-hidden">Open event</span>
+          </button>
+        </header>
+        ${offersList}
+      </form>`;
   }
 
   _closeHandler(evt) {
@@ -142,6 +64,217 @@ export class EventEditView extends AbstractView {
     this._callback.submit();
   }
 
+  _toggleFavoriteHandler(evt) {
+    evt.preventDefault();
+    this._callback.toggleFavorite();
+  }
+
+  _makeEventTypeSelector() {
+    const {id, type} = this._event;
+
+    let template = `
+      <div class="event__type-wrapper">
+        <label class="event__type  event__type-btn" for="event-type-toggle-${id}">
+          <span class="visually-hidden">Choose event type</span>
+          <img
+            class="event__type-icon"
+            width="17" height="17"
+            src="img/icons/${type.toLowerCase()}.png"
+            alt="Event type icon">
+        </label>
+        <input
+          class="event__type-toggle  visually-hidden"
+          id="event-type-toggle-${id}"
+          type="checkbox">
+        <div class="event__type-list">
+          <fieldset class="event__type-group">
+            <legend class="visually-hidden">Transfer</legend>`;
+
+    for (const transferName of TRANSPORTS) {
+      template += `
+            <div class="event__type-item">
+              <input
+                id="event-type-${transferName.toLowerCase()}-1"
+                class="event__type-input  visually-hidden"
+                type="radio"
+                name="event-type"
+                value="${transferName.toLowerCase()}"
+                ${(transferName === type) ? `checked` : ``}>
+              <label
+                class="event__type-label  event__type-label--${transferName.toLowerCase()}"
+                for="event-type-${transferName.toLowerCase()}-1">${transferName}</label>
+            </div>`;
+    }
+
+    template += `
+          </fieldset>
+          <fieldset class="event__type-group">
+            <legend class="visually-hidden">Activity</legend>`;
+
+    for (const activityName of STOPS) {
+      template += `
+            <div class="event__type-item">
+              <input
+                id="event-type-${activityName.toLowerCase()}-1"
+                class="event__type-input  visually-hidden"
+                type="radio" name="event-type"
+                value="${activityName.toLowerCase()}"
+                ${(activityName === type) ? `checked` : ``}>
+              <label
+                class="event__type-label  event__type-label--${activityName.toLowerCase()}"
+                for="event-type-${activityName.toLowerCase()}-1">
+                  ${activityName}
+              </label>
+            </div>`;
+    }
+
+    template += `
+          </fieldset>
+        </div>
+      </div>`;
+
+    return template;
+  }
+
+  _makeCitySelector() {
+    const {id, type, city} = this._event;
+    const isTransfer = TRANSPORTS.indexOf(type) >= 0;
+
+    let template = `
+      <div class="event__field-group  event__field-group--destination">
+        <label class="event__label  event__type-output" for="event-destination-${id}">
+          ${type} ${(isTransfer) ? `to` : `in`}
+        </label>
+        <input
+            class="event__input  event__input--destination"
+            id="event-destination-${id}"
+            type="text"
+            name="event-destination"
+            value="${city}"
+            list="destination-list-1">
+        <datalist id="destination-list-${id}">`;
+
+    CITIES.forEach((cityName) => {
+      template += `<option value="${cityName}"></option>`;
+    });
+
+    template += `
+        </datalist>
+      </div>`;
+
+    return template;
+  }
+
+  _makeTimeInput() {
+    const {id} = this._event;
+
+    const beginMonth = this._event.beginTime.toLocaleDateString(
+        `en-GB`,
+        {year: `2-digit`, month: `2-digit`, day: `2-digit`}
+    );
+    const beginTime = this._event.beginTime.toLocaleTimeString(
+        `en-GB`,
+        {timeStyle: `short`, hour12: false}
+    );
+    const endMonth = this._event.endTime.toLocaleDateString(
+        `en-GB`,
+        {year: `2-digit`, month: `2-digit`, day: `2-digit`}
+    );
+    const endTime = this._event.endTime.toLocaleTimeString(
+        `en-GB`,
+        {timeStyle: `short`, hour12: false}
+    );
+
+    return `
+      <div class="event__field-group  event__field-group--time">
+        <label class="visually-hidden" for="event-start-time-${id}">From</label>
+        <input
+            class="event__input  event__input--time"
+            id="event-start-time-${id}"
+            type="text"
+            name="event-start-time"
+            value="${beginMonth} ${beginTime}">
+        &mdash;
+        <label class="visually-hidden" for="event-end-time-${id}">To</label>
+        <input
+            class="event__input  event__input--time"
+            id="event-end-time-${id}"
+            type="text"
+            name="event-end-time"
+            value="${endMonth} ${endTime}">
+      </div>`;
+  }
+
+  _makePriceInput() {
+    const {id, price} = this._event;
+    return `
+      <div class="event__field-group  event__field-group--price">
+        <label class="event__label" for="event-price-${id}">
+          <span class="visually-hidden">Price</span>
+          &euro;
+        </label>
+        <input
+            class="event__input  event__input--price"
+            id="event-price-${id}"
+            type="text"
+            name="event-price"
+            value="${price}">
+      </div>`;
+  }
+
+  _makeFavoriteButton() {
+    const {id, isFavorite} = this._event;
+
+    return `
+      <input
+          id="event-favorite-${id}"
+          class="event__favorite-checkbox  visually-hidden"
+          type="checkbox"
+          name="event-favorite" ${(isFavorite) ? `checked` : ``}>
+      <label class="event__favorite-btn" for="event-favorite-${id}">
+      <span class="visually-hidden">Add to favorite</span>
+        <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
+          <path
+              d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688
+              14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
+        </svg>
+      </label>`;
+  }
+
+  _makeOffersList() {
+    const {id, type, offers} = this._event;
+    const offersList = (TRANSPORTS.indexOf(type) >= 0) ? TRANSPORT_OFFERS_MAP : STOP_OFFERS_MAP;
+
+    let template = `
+        <section class="event__details">
+          <section class="event__section  event__section--offers">
+            <h3 class="event__section-title  event__section-title--offers">Offers</h3>
+            <div class="event__available-offers">`;
+
+    for (const [offerId, offerDetails] of offersList) {
+      template += `
+              <div class="event__offer-selector">
+                <input
+                    class="event__offer-checkbox  visually-hidden"
+                    id="event-offer-${offerId}-${id}"
+                    type="checkbox"
+                    name="event-offer-${offerId}" ${(offers[offerId]) ? `checked` : ``}>
+                <label class="event__offer-label" for="event-offer-${offerId}-${id}">
+                  <span class="event__offer-title">${offerDetails.title}</span>
+                  &plus;
+                  &euro;&nbsp;<span class="event__offer-price">${offerDetails.price}</span>
+                </label>
+              </div>`;
+    }
+
+    template += `
+            </div>
+          </section>
+        </section>`;
+
+    return template;
+  }
+
   set closeHandler(callback) {
     this._callback.close = callback;
 
@@ -152,5 +285,11 @@ export class EventEditView extends AbstractView {
     this._callback.submit = callback;
 
     this.element.addEventListener(`submit`, this._submitHandler);
+  }
+
+  set toggleFavoriteHandler(callback) {
+    this._callback.toggleFavorite = callback;
+
+    this.element.querySelector(`.event__favorite-btn`).addEventListener(`click`, this._toggleFavoriteHandler);
   }
 }
--- src/view/event-summary-view.js
+++ src/view/event-summary-view.js
@@ -1,3 +1,4 @@
+import {TRANSPORTS} from "../const.js";
 import {AbstractView} from "./abstract-view.js";
 import {Itinerary} from "../utils/itinerary.js";
 
@@ -12,46 +13,90 @@ export class EventSummaryView extends AbstractView {
   }
 
   get template() {
-    const {type, isTransfer, city, beginTime, endTime, price, offers} = this._event;
+    const {type, city} = this._event;
+    const isTransfer = TRANSPORTS.indexOf(type) >= 0;
+    const priceBlock = this._makePriceBlock();
+    const offerList = this._makeOffersList();
+    const scheduleBlock = this._makeScheduleBlock();
+
     let template = `<div class="event">
             <div class="event__type">
-              <img class="event__type-icon" width="42" height="42" src="img/icons/${type.toLowerCase()}.png" alt="Event type icon">
+              <img
+                  class="event__type-icon"
+                  width="42" height="42"
+                  src="img/icons/${type.toLowerCase()}.png"
+                  alt="Event type icon">
             </div>
             <h3 class="event__title">${type} ${(isTransfer) ? `to` : `in`} ${city}</h3>
 
-            <div class="event__schedule">
-              <p class="event__time">
-                <time class="event__start-time" datetime="${beginTime.toISOString()}">${beginTime.toLocaleTimeString(`en-US`, {timeStyle: `short`, hour12: false})}</time>
-                &mdash;
-                <time class="event__end-time" datetime="${endTime.toISOString()}">${endTime.toLocaleTimeString(`en-US`, {timeStyle: `short`, hour12: false})}</time>
-              </p>
-              <p class="event__duration">${Itinerary.getDuration(beginTime, endTime)}</p>
-            </div>
+            ${scheduleBlock}
 
-            <p class="event__price">
-              &euro;&nbsp;<span class="event__price-value">${price}</span>
-            </p>`;
+            ${priceBlock}
 
-    const offerList = Object.values(offers);
-    if (offerList.length > 0) {
-      template += `<h4 class="visually-hidden">Offers:</h4>
-            <ul class="event__selected-offers">`;
+            ${offerList}
 
-      for (let i = 0; i < this._MAX_OFFERS_TO_SHOW && i < offerList.length; i++) {
-        template += `<li class="event__offer">
+            <button class="event__rollup-btn" type="button">
+              <span class="visually-hidden">Open event</span>
+            </button>
+          </div>`;
+
+    return template;
+  }
+
+  _makeScheduleBlock() {
+    const beginDateTime = this._event.beginTime.toISOString();
+    const beginTime = this._event.beginTime.toLocaleTimeString(
+        `en-US`,
+        {timeStyle: `short`, hour12: false}
+    );
+
+    const endDateTime = this._event.endTime.toISOString();
+    const endTime = this._event.endTime.toLocaleTimeString(
+        `en-US`,
+        {timeStyle: `short`, hour12: false}
+    );
+
+    const duration = Itinerary.getDuration(this._event.beginTime, this._event.endTime);
+
+    return `<div class="event__schedule">
+            <p class="event__time">
+              <time class="event__start-time" datetime="${beginDateTime}">${beginTime}</time>
+              &mdash;
+              <time class="event__end-time" datetime="${endDateTime}">${endTime}</time>
+            </p>
+            <p class="event__duration">${duration}</p>
+          </div>`;
+  }
+
+  _makePriceBlock() {
+    const {price} = this._event;
+
+    return `<p class="event__price">
+          &euro;&nbsp;<span class="event__price-value">${price}</span>
+        </p>`;
+  }
+
+  _makeOffersList() {
+    const offerList = Object.values(this._event.offers);
+    if (offerList.length === 0) {
+      return ``;
+    }
+
+    let template = `
+        <h4 class="visually-hidden">Offers:</h4>
+        <ul class="event__selected-offers">`;
+
+    for (let i = 0; i < this._MAX_OFFERS_TO_SHOW && i < offerList.length; i++) {
+      template += `
+          <li class="event__offer">
             <span class="event__offer-title">${offerList[i].title}</span>
             &plus;
             &euro;&nbsp;<span class="event__offer-price">${offerList[i].price}</span>
           </li>`;
-      }
-
-      template += `</ul>`;
     }
 
-    template += `<button class="event__rollup-btn" type="button">
-              <span class="visually-hidden">Open event</span>
-            </button>
-          </div>`;
+    template += `
+        </ul>`;
 
     return template;
   }
